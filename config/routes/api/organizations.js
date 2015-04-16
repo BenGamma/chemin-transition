fca15a7615dbd1ae647bc8f82dd28ed618ece51a
SamiErrighi <sami.errighi@gmail.com>
2015-04-16 13:49:28 +0200

    fix bug upload images

--- config/routes/api/organizations.js
+++ config/routes/api/organizations.js
@@ -12,8 +12,9 @@ router.route('/show/:organization').get(organizations.show);
 router.route('/profile').get(authorization.requiresLogin, organizations.profile);
 router.route('/actor/:organization/:person').post(actorAuthorization.checkOrganization, organizations.addActor);
 router.route('/addSkill').post(organizations.addSkill);
-router.route('/images').post(authorization.requiresLogin, organizations.addImage)
-router.route('/images').delete(authorization.requiresLogin, organizations.removeImage)
+router.route('/images')
+    .post(authorization.requiresLogin, organizations.addImage)
+    .delete(authorization.requiresLogin, organizations.removeImage)
 router.route('/actor/:organization/:id').delete(actorAuthorization.checkOrganization, organizations.removeActor);
 router.route('/update/:organization').put(authorization.requiresLogin, actorAuthorization.checkOrganization, actorAuthorization.addSkills, organizations.update);
 
